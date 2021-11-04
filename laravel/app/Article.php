c204adab92d21ea47423d03eaf673d407e2d8191
kenji.iwamoto <>
2021-11-04 11:42:07 +0900

    記事登録機能

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -3,16 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-//==========ここから追加==========
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-//==========ここまで追加==========
 
 class Article extends Model
 {
-    //==========ここから追加==========
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
     }
-    //==========ここまで追加==========
 }
